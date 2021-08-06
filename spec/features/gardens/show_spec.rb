6bab5a45ee5cd11a7bc9cfaaf0846d7e9c61c047
Lee Hopper <hopper.lee.j@gmail.com>
2021-08-06 10:42:45 -0600

    Build test for plants display on garden show page and then build display

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -26,15 +26,46 @@ RSpec.describe 'the garden show' do
   end
 
   describe 'display' do
-    it 'shows all of the unique plants in the garden that take less than 100 days to harvest and ordered number of plants in plots from most to least' do
+    it 'shows unique plants in the garden in descending order by number of times in plots' do
       visit garden_path(@garden_1.id)
 
       within("#header") do
         expect(page).to have_content("#{@garden_1.name} Plants")
       end
 
-      # within("#plants") do
-      # end
+      within("#plants") do
+        first = find("#plant-#{@plant_3.id}")
+        second = find("#plant-#{@plant_4.id}")
+        third = find("#plant-#{@plant_1.id}")
+
+        expect(first).to appear_before(second)
+        expect(second).to appear_before(third)
+
+        within("#plant-#{@plant_3.id}") do
+          expect(page).to have_content("1. #{@plant_3.name}")
+        end
+
+        within("#plant-#{@plant_4.id}") do
+          expect(page).to have_content("2. #{@plant_4.name}")
+        end
+
+        within("#plant-#{@plant_1.id}") do
+          expect(page).to have_content("3. #{@plant_1.name}")
+        end
+      end
+    end
+
+    it 'does not display plants in another garden or plants with 100 or more days to harvest' do
+      garden_2 = Garden.create!(name: 'Denver Garden', organic: true)
+      plot_4 = garden_2.plots.create!(number: 10, size: 'Large', direction: 'North')
+      plant_6 = plot_4.plants.create!(name: 'Mint', description: 'spearmint', days_to_harvest: 20)
+
+      visit garden_path(@garden_1.id)
+
+      expect(page).to_not have_content(@plant_2.name)
+      expect(page).to_not have_content(@plant_5.name)
+      expect(page).to_not have_content(plant_6.name)
+      expect(page).to_not have_content(garden_2.name)
     end
   end
 end
